fb17d041464bd34cdfdb6c8025d22f2c3b819def
Kr31nZss <Kr31nZss210907@mail.ru>
2024-05-05 18:46:52 +0600

    почти готово

--- src/assets/getChangeValue.jsx
+++ src/assets/getChangeValue.jsx
@@ -1,48 +1,63 @@
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function GetChangeValue() {
-    const [exchangeRateUSD, setExchangeRateUSD] = useState(null);
-    const [semiValue , setSemiValue] = useState(0);
-    const [Value , setValue] = useState(0);
-
-    const baseCurrency = 'USD'; 
+    const [exchangeRate, setExchangeRate] = useState(null);
+    const [firstValue , setFirstValue] = useState("");
+    const [secondValue , setSecondValue] = useState("");
+    const [userValueNums , setUserValueNums] = useState("");
+    const [result , setResult] = useState(null);
 
     useEffect(() => {
-        async function fetchExchangeRates() {
+        async function axiosExchangeRates() {
             const apiKey = '96470b4a535d49f1ebe49148';
-            const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCurrency}`;
+            const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${firstValue}`;
         
             try {
-                const response = await fetch(apiUrl);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch exchange rates');
-                }
-                const data = await response.json();
-                setExchangeRateUSD(data.conversion_rates.KGS);
+                const response = await axios.get(apiUrl);
+                const data = response.data;
+                setExchangeRate(data.conversion_rates[secondValue]);
             } catch (error) {
                 console.error(error);
             }
         }
-        fetchExchangeRates();
-    }, [baseCurrency]);
+        axiosExchangeRates();
+    }, [firstValue , secondValue ,userValueNums]);
 
-    const getValueFromInp = (click) => {
-        if (click.key === "Enter") {
-            setExchangeRateUSD( exchangeRateUSD * semiValue);
+    const getValueFromInp = (event) => {
+        if (event.key === "Enter") {
+            
         }
     }
 
-    console.log(+semiValue);
+    useEffect(() => {
+        setResult(exchangeRate * userValueNums);
+    }, [exchangeRate, userValueNums]);
+
     return (
         <div>
-            <input type='text'
-            placeholder='сколько?' 
-            onChange={e => setSemiValue(e.target.value)} 
-            onKeyDown={getValueFromInp}/>
-            <p>Курс доллара к рублю: {Math.round(exchangeRateUSD)}</p>
+            <input 
+            type='text'
+            placeholder='сколько у вас?' 
+            value={userValueNums}
+            onChange={e => setUserValueNums(e.target.value)} 
+            />
+            <input 
+            type='text'
+            placeholder='какая у вас валюта?'
+            value={firstValue}
+            onChange={e => setFirstValue(e.target.value)} 
+            />
+            <input
+            onKeyDown={getValueFromInp} 
+            type='text'
+            placeholder='в какую перевести?'
+            value={secondValue}
+            onChange={e => setSecondValue(e.target.value)} 
+            />
+            <p>Курс {firstValue} к {secondValue}: {Math.round(result)}</p>
         </div>
     );
 }
 
 export default GetChangeValue;
-
